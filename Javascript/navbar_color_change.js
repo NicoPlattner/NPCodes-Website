02d1bdc757b4efce177f251903ff3537c9791f8e
Nico <if17b023@technikum-wien.at>
2019-02-07 13:49:45 +0100

    changed some gifs to videos for performance

--- Javascript/navbar_color_change.js
+++ Javascript/navbar_color_change.js
@@ -12,3 +12,13 @@ $(function() {
         }
     });
 });
+
+$(document).ready(function () {
+    $(".box").hover(function () {
+        $(this).children("video")[0].play();
+    }, function () {
+        var el = $(this).children("video")[0];
+        el.pause();
+        el.currentTime = 0;
+    });
+});
